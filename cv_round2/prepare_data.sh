ab5fd3743b121e59c4cb69875490bbb920f4ae79
sneakerkg <xiaotj1990327@gmail.com>
2021-07-21 13:36:06 +0000

    data prepare for cv round2

---
+++ cv_round2/prepare_data.sh
@@ -0,0 +1,16 @@
+mkdir data
+cd data
+wget http://images.cocodataset.org/zips/train2017.zip
+unzip train2017.zip
+rm train2017.zip
+mv train2017 train
+wget http://images.cocodataset.org/zips/val2017.zip
+unzip val2017.zip
+rm val2017.zip
+mv val2017 val
+wget https://dgl-data.s3.us-west-2.amazonaws.com/dataset/hackathon2021/keypoint_test.json
+wget https://dgl-data.s3.us-west-2.amazonaws.com/dataset/hackathon2021/keypoint_train_gt.json
+wget https://dgl-data.s3.us-west-2.amazonaws.com/dataset/hackathon2021/keypoint_val_gt.json
+mkdir test
+python prepare_test_set.py
+cd ..
